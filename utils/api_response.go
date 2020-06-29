5ad3d1db7d4dbf8664550e110f4e7afea55e3a4d
ikyilmaz <ismailkurbanyilmaz@gmail.com>
2020-06-29 16:14:07 +0300

    Create APIResponse

---
+++ utils/api_response.go
@@ -0,0 +1,18 @@
+package utils
+
+import "time"
+
+type APIResponse struct {
+	Status     string      `json:"status"`
+	StatusCode int         `json:"statusCode"`
+	Data       interface{} `json:"data"`
+	Timestamp  int64       `json:"timestamp"`
+}
+
+func NewAPIResponse(statusCode int, data interface{}) *APIResponse {
+	return &APIResponse{"success", statusCode, data, time.Now().Unix()}
+}
+
+func OK(data interface{}) *APIResponse        { return NewAPIResponse(200, data) }
+func Created(data interface{}) *APIResponse   { return NewAPIResponse(201, data) }
+func NoContent(data interface{}) *APIResponse { return NewAPIResponse(204, data) }
