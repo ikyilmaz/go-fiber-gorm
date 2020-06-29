dddd548bdcc68ea4cc690f68f27bf798d7e8a615
ikyilmaz <ismailkurbanyilmaz@gmail.com>
2020-06-29 20:02:48 +0300

    Improve UserModel

--- models/user_model.go
+++ models/user_model.go
@@ -2,11 +2,11 @@ package models
 
 type UserModel struct {
 	BaseModel
-	FirstName string
-	LastName  string
-	Username  string
-	Email     string
-	Password  string
+	FirstName string `gorm:"type:varchar(32);"`
+	LastName  string `gorm:"type:varchar(32);"`
+	Username  string `gorm:"type:varchar(32);not null;unique;"`
+	Email     string `gorm:"type:varchar(128);not null;unique;"`
+	Password  string `gorm:"type:varchar(255);not null;unique;"`
 }
 
 func (u UserModel) TableName() string { return "users" }
