f3212dd6e2eb3e31011ad6d5de71345693ae82a2
ikyilmaz <ismailkurbanyilmaz@gmail.com>
2020-06-29 20:03:04 +0300

    Create user_responses.go

---
+++ responses/user_responses.go
@@ -0,0 +1,21 @@
+package responses
+
+import "time"
+
+type UserCreatedPrivate struct {
+	FirstName string    `json:"firstName"`
+	LastName  string    `json:"lastName"`
+	Username  string    `json:"username"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"deletedAt"`
+}
+
+// without email and updated at fields
+type UserCreatedPublic struct {
+	ID        uint      `json:"id"`
+	FirstName string    `json:"firstName"`
+	LastName  string    `json:"lastName"`
+	Username  string    `json:"username"`
+	CreatedAt time.Time `json:"createdAt"`
+}
