a7feefa8782b08becaa73246712ab34fa33854f1
ikyilmaz <ismailkurbanyilmaz@gmail.com>
2020-06-29 20:01:59 +0300

    Added handleError functions

--- middlewares/error_middleware.go
+++ middlewares/error_middleware.go
@@ -8,8 +8,19 @@ import (
 func ErrorHandler(c *fiber.Ctx, err error) {
 	switch err.(type) {
 	case *utils.APIError:
-		c.JSON(err.(*utils.APIError))
+		handleAPIError(c, err.(*utils.APIError))
+	case *utils.DBError:
+		handleDBError(c, err.(*utils.DBError))
 	default:
 		c.JSON(utils.NewAPIError(500, "something went wrong"))
 	}
+}
+
+func handleAPIError(c *fiber.Ctx, err *utils.APIError) {
+	c.JSON(err)
+}
+
+// TODO
+func handleDBError(c *fiber.Ctx, err *utils.DBError) {
+	c.JSON(utils.NewAPIError(500, err.Error()))
 }
