93c7295ca96ec9ea06c0aa3a43ce674e6df4ee34
ikyilmaz <ismailkurbanyilmaz@gmail.com>
2020-06-29 20:03:29 +0300

    Implement service for endpoint CreateUser

--- services/user_service.go
+++ services/user_service.go
@@ -1,6 +1,12 @@
 package services
 
-import "gorm.io/gorm"
+import (
+	"fiber-rest-api/forms"
+	"fiber-rest-api/models"
+	"fiber-rest-api/responses"
+	"fiber-rest-api/utils"
+	"gorm.io/gorm"
+)
 
 type UserService struct{ db *gorm.DB }
 
@@ -10,6 +16,26 @@ func (u *UserService) GetManyUser() {
 	panic("implement me")
 }
 
-func (u *UserService) CreateUser() {
-	panic("implement me")
+func (u *UserService) CreateUser(createUserForm *forms.CreateUser) (*responses.UserCreatedPublic, *utils.DBError) {
+	db := u.db
+
+	userModel := models.UserModel{
+		FirstName: createUserForm.FirstName,
+		LastName:  createUserForm.LastName,
+		Username:  createUserForm.Username,
+		Email:     createUserForm.Email,
+		Password:  createUserForm.Password,
+	}
+
+	if err := db.Create(&userModel).Error; err != nil {
+		return nil, utils.NewDBError(err)
+	}
+
+	return &responses.UserCreatedPublic{
+		ID:        userModel.ID,
+		FirstName: userModel.FirstName,
+		LastName:  userModel.LastName,
+		Username:  userModel.Username,
+		CreatedAt: userModel.CreatedAt,
+	}, nil
 }
