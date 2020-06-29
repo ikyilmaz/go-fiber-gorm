db2435f4d736ea223074851e86f5b8fb5e34d25f
ikyilmaz <ismailkurbanyilmaz@gmail.com>
2020-06-29 20:01:16 +0300

    added StatusText field

--- utils/api_response.go
+++ utils/api_response.go
@@ -1,16 +1,20 @@
 package utils
 
-import "time"
+import (
+	"net/http"
+	"time"
+)
 
 type APIResponse struct {
 	Status     string      `json:"status"`
 	StatusCode int         `json:"statusCode"`
+	StatusText string      `json:"statusText"`
 	Data       interface{} `json:"data"`
 	Timestamp  int64       `json:"timestamp"`
 }
 
 func NewAPIResponse(statusCode int, data interface{}) *APIResponse {
-	return &APIResponse{"success", statusCode, data, time.Now().Unix()}
+	return &APIResponse{"success", statusCode, http.StatusText(statusCode), data, time.Now().Unix()}
 }
 
 func OK(data interface{}) *APIResponse        { return NewAPIResponse(200, data) }
