3a5d61a3969cb1b562f61dea85443dcc2c34cbff
ikyilmaz <ismailkurbanyilmaz@gmail.com>
2020-07-05 16:29:45 +0300

    Create ValidateUpdateUserForm validator

--- validators/user_validators.go
+++ validators/user_validators.go
@@ -24,3 +24,20 @@ func ValidateCreateUserForm(c *fiber.Ctx) {
 
 	c.Next()
 }
+
+func ValidateUpdateUserForm(c *fiber.Ctx) {
+	body := []byte(c.Body())
+
+	_, err := jio.ValidateJSON(&body, jio.Object().Keys(jio.K{
+		"firstName": jio.String().Optional().Min(2).Max(32),
+		"lastName":  jio.String().Optional().Min(2).Max(32),
+		"username":  jio.String().Optional().Min(2).Max(32),
+	}).Required())
+
+	if err != nil {
+		c.Next(utils.NewAPIError(400, err.Error()))
+		return
+	}
+
+	c.Next()
+}
